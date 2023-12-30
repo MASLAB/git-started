81b2bf45b6e68de31722fa771037acb0f039e5ca
Joseph Hobbs <josephrosshobbs@gmail.com>
2023-12-30 17:06:55 -0500

    Added test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def failure():
+    print("Oh no!  There's a bug!")
+    print("[ERROR] `test.py` failed")
+
+def success():
+    print("Yay!  You fixed the bug!")
+    print("`test.py` completed with no errors")
+
+if __name__ == "__main__":
+    failure()
